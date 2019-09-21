777817d97c0e856d5d582413ae0480932daadac7
olavurellefsen <oe@tokni.com>
2019-09-21 15:56:38 +0100

    Update with initial privacy policy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `HeyBreyd Privacy Policy`,
+    description: `This is the privacy policy of the HeyBreyd app.`,
+    author: `@gaman`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
